38a92788490ced59906c942a94408bcabd9d8d8e
Preetesh8485 <preeteshkhadanga2005@gmail.com>
2026-06-02 10:14:12 +0530

    onrender removal

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,18 +1,11 @@
-import SibApiV3Sdk from "@getbrevo/brevo";
-const { TransactionalEmailsApi, SendSmtpEmail, TransactionalEmailsApiApiKeys } = SibApiV3Sdk;
 
-const apiInstance = new TransactionalEmailsApi();
-apiInstance.setApiKey(
-  TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY
-);
-
-export const sendEmail = async ({ email, subject, message }) => {
-  const sendSmtpEmail = new SendSmtpEmail();
-  sendSmtpEmail.sender = { email: process.env.SENDER_EMAIL };
-  sendSmtpEmail.to = [{ email }];
-  sendSmtpEmail.subject = subject;
-  sendSmtpEmail.htmlContent = message;
-
-  await apiInstance.sendTransacEmail(sendSmtpEmail);
-};
+import transporter from "../config/nodemailer.js";
+export const sendEmail= async({email,subject,message})=>{
+    const mailOptions = {
+                from: process.env.SENDER_EMAIL,
+                to: email,
+                subject,
+                html: message
+            }
+            await transporter.sendMail(mailOptions);
+}
--- Backend/utils/sendVerificationCode.js
+++ Backend/utils/sendVerificationCode.js
@@ -1,24 +1,18 @@
-import SibApiV3Sdk from "@getbrevo/brevo";
-const { TransactionalEmailsApi, SendSmtpEmail, TransactionalEmailsApiApiKeys } = SibApiV3Sdk;
+import transporter from "../config/nodemailer.js";
 import { EMAIL_VERIFY_TEMPLATE } from "./emailTemplates.js";
-
-const apiInstance = new TransactionalEmailsApi();
-apiInstance.setApiKey(
-  TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY
-);
 
 export async function SendVerificationCode(verificationCode, email, res) {
     try {
-        const sendSmtpEmail = new SendSmtpEmail();
-        sendSmtpEmail.sender = { email: process.env.SENDER_EMAIL };
-        sendSmtpEmail.to = [{ email }];
-        sendSmtpEmail.subject = "Library Account verification OTP";
-        sendSmtpEmail.htmlContent = EMAIL_VERIFY_TEMPLATE
-            .replace("{{otp}}", verificationCode)
-            .replace("{{email}}", email);
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: "Library Account verification OTP",
+            html: EMAIL_VERIFY_TEMPLATE
+                .replace("{{otp}}", verificationCode)
+                .replace("{{email}}", email)
+        };
 
-        await apiInstance.sendTransacEmail(sendSmtpEmail);
+        await transporter.sendMail(mailOptions);
 
         return res.json({
             success: true,
