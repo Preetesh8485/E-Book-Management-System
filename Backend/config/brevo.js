0712590311fb5af56f3cb9b7bb5adaf4fd767863
Preetesh8485 <preeteshkhadanga2005@gmail.com>
2026-06-02 10:28:25 +0530

    onrender smtp changed

--- Backend/config/brevo.js
+++ Backend/config/brevo.js
@@ -1,7 +1,10 @@
-import Brevo from "@getbrevo/brevo";
+import * as Brevo from "@getbrevo/brevo";
+
 const apiInstance = new Brevo.TransactionalEmailsApi();
+
 apiInstance.setApiKey(
-  Brevo.TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY
+    Brevo.TransactionalEmailsApiApiKeys.apiKey,
+    process.env.BREVO_API_KEY
 );
+
 export default apiInstance;
